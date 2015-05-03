43875ad8b3f21146c86f13b3d019db81b432b5e1
Joshua Belanger <jcbelanger@users.noreply.github.com>
2015-05-03 12:43:43 -0400

    Update Challenge #212 [Hard] Reverse Maze Pathfinding.hs

--- Challenge #212 [Hard] Reverse Maze Pathfinding.hs
+++ Challenge #212 [Hard] Reverse Maze Pathfinding.hs
@@ -11,7 +11,7 @@ import Control.Applicative
 import Control.Monad.State
 import Text.Printf
 
-data Action = TurnLeft | TurnRight | Forward Int
+data Action = TurnLeft | TurnRight | Forward Int deriving Show
 data Direction = North | East | South | West deriving (Enum, Bounded)
 type MazeState = (Pos, Direction)
 type Pos = (Int, Int)
@@ -22,20 +22,20 @@ main = interact $ \input ->
         spaces = Set.fromList [ (x, y) 
                               | (y, line) <- zip [0..] maze
                               , (x, ' ')  <- zip [0..] line ]
-        steps = parseActions directions
-    in unlines [ printf "From %s to %s" (show startPos) (show endPos)
-               | startPos <- Set.toList spaces
-               , startDir <- [minBound..maxBound]
-               , let (path, (endPos, endDir)) = runSteps steps startPos startDir
-               , path `Set.isSubsetOf` spaces ]
+        steps = optimizePath $ parseActions directions
+    in unlines $ nub [ printf "From %s to %s" (show startPos) (show endPos)
+                     | startPos <- Set.toList spaces
+                     , startDir <- [minBound..maxBound]
+                     , let (path, (endPos, endDir)) = runSteps steps (startPos, startDir)
+                     , path `Set.isSubsetOf` spaces ]
 
-parseActions = map parse . groupBy ((&&) `on` isDigit) where
-    parse "l" = TurnLeft
-    parse "r" = TurnRight
-    parse  n  = Forward (read n)
+parseActions = map parseAction . groupBy ((&&) `on` isDigit) where
+    parseAction "l" = TurnLeft
+    parseAction "r" = TurnRight
+    parseAction  n  = Forward (read n)
 
-runSteps :: [Action] -> Pos -> Direction -> (Set Pos, MazeState)
-runSteps steps startPos startDir = runState stepSpaces (startPos, startDir) where
+runSteps :: [Action] -> MazeState -> (Set Pos, MazeState)
+runSteps steps start@(startPos, startDir) = runState stepSpaces start where
     stepSpaces = foldM act (Set.singleton startPos) steps
 
 act :: Set Pos -> Action -> State MazeState (Set Pos)
@@ -48,7 +48,7 @@ turn delta = modify $ \(pos, dir) ->
     let curIx = fromEnum dir
         newIx = (curIx + delta) `mod` 4
         newDir = toEnum newIx
-    in  (pos, newDir)
+    in (pos, newDir)
 
 step :: State MazeState ()
 step = modify $ \((x,y), dir) ->
@@ -59,3 +59,12 @@ step = modify $ \((x,y), dir) ->
             West  -> (x-1, y)
     in (newPos, dir)
 
+optimizePath :: [Action] -> [Action]
+optimizePath = foldr optimize [] where
+    optimize (Forward a) (Forward b:xs)           = Forward (a+b):xs
+    optimize (Forward 0) xs                       = xs
+    optimize TurnLeft    (TurnRight:xs)           = xs
+    optimize TurnRight   (TurnLeft:xs)            = xs
+    optimize TurnRight   (TurnRight:TurnRight:xs) = TurnLeft:xs
+    optimize TurnLeft    (TurnLeft:TurnLeft:xs)   = TurnRight:xs
+    optimize x xs                                 = x:xs
