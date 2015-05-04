c92bd662047430c55c7cb9be84db78fccdbc67a4
Joshua Belanger <jcbelanger@users.noreply.github.com>
2015-05-04 04:12:01 -0400

    Update Challenge #213 [Easy] Pronouncing Hex.hs

--- Challenge #213 [Easy] Pronouncing Hex.hs
+++ Challenge #213 [Easy] Pronouncing Hex.hs
@@ -2,25 +2,15 @@
 http://www.reddit.com/r/dailyprogrammer/comments/34rxkc/20150504_challenge_213_easy_pronouncing_hex/
 -}
 
-{-# LANGUAGE FlexibleInstances #-} 
-
-import Data.Monoid
-
-class Monoid a => Seminearring a where
-  mone  :: a
-  mdist :: a -> a -> a
-
-instance Seminearring [ [a] ] where
-  mone = [[]]
-  xss `mdist` yss = [ xs ++ ys | xs <- xss, ys <- yss ]
+xss <.> yss = [ xs ++ ys | xs <- xss, ys <- yss ]
 
 dig1 = words "one two three four five six seven eight nine a bee cee dee e eff"
 dig2 = dig1
-       <> words "ten eleven twelve"
-       <> words "thir four fif six seven eigh nine ab bib cleven dibble egg fleven" `mdist` ["teen"]
-       <> (twentyToNinety <> ["atta"] <> bibbityToFleventy) `mdist` (mone <> (["-"] `mdist` dig1))
-       where twentyToNinety = words "twen thir for fif six seven eigh nine" `mdist` ["ty"]
-             bibbityToFleventy = words "bibbi ci dicke ebbi fleven" `mdist` ["ty"]
-dig4 = dig2 <> dig2 `mdist` [" bitey"] `mdist` (mone <> ([" "] `mdist` dig2))
+       ++ words "ten eleven twelve"
+       ++ words "thir four fif six seven eigh nine ab bib cleven dibble egg fleven" <.> ["teen"]
+       ++ (twentyToNinety ++ ["atta"] ++ bibbityToFleventy) <.> ([] : (["-"] <.> dig1))
+       where twentyToNinety = words "twen thir for fif six seven eigh nine" <.> ["ty"]
+             bibbityToFleventy = words "bibbi ci dicke ebbi fleven" <.> ["ty"]
+dig4 = dig2 ++ dig2 <.> [" bitey"] <.> ([] : ([" "] <.> dig2))
 
 main = interact $ (("zero":dig4) !!) . read
