06091e0b5766fc3a7e4c06bd5b5d7bcea0dfbec6
Joshua Belanger <jcbelanger@users.noreply.github.com>
2015-05-21 23:25:41 -0400

    Create Challenge #215 [Intermediate] Validating sorting networks.hs

---
+++ Challenge #215 [Intermediate] Validating sorting networks.hs
@@ -0,0 +1,20 @@
+{-
+http://www.reddit.com/r/dailyprogrammer/comments/36m83a/20150520_challenge_215_intermediate_validating/
+-}
+
+import qualified Data.Vector as V
+
+main = interact $ \input ->
+    let [wordCount, _]:compares = [map read (words line) | line <- lines input]
+        allSorted = all isSorted [ foldl runCompare testNum compares
+                                 | testNum <- V.replicateM wordCount [0,1] ]
+    in case allSorted of
+        True  -> "Valid network"
+        False -> "Invalid network"
+
+isSorted xs = V.and $ V.zipWith (<=) xs (V.drop 1 xs)
+
+runCompare testNum [startIx, endIx] = do
+    a <- V.indexM testNum startIx
+    b <- V.indexM testNum endIx
+    testNum V.// [ (startIx, min a b), (endIx, max a b) ]
