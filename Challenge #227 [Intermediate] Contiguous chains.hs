079567a198ec52226884b98a0e4bd33b0b2db9f8
Joshua Belanger <jcbelanger@users.noreply.github.com>
2015-08-12 21:50:25 -0400

    Update Challenge #227 [Intermediate] Contiguous chains.hs

--- Challenge #227 [Intermediate] Contiguous chains.hs
+++ Challenge #227 [Intermediate] Contiguous chains.hs
@@ -1,8 +1,9 @@
 import Data.Graph
 
-main = interact challenge where
-    challenge input | (graph,_,_) <- graphFromEdges 
-        [ ((), (x,y), [(x+1,y),(x-1,y),(x,y+1),(x,y-1)])
-        | (y, line) <- zip [1..] (lines input)
-        , (x, 'x')  <- zip [1..] line ]
-        = show . length $ components graph
+main = interact challenge
+
+challenge input | (graph,_,_) <- graphFromEdges 
+    [ ((), (x,y), [(x+1,y),(x-1,y),(x,y+1),(x,y-1)])
+    | (y, line) <- zip [1..] (lines input)
+    , (x, 'x')  <- zip [1..] line ]
+    = show . length $ components graph
