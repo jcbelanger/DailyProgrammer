a2f8a5bb12e0b455bac5122994cbf1758bf58fd3
Joshua Belanger <jcbelanger@users.noreply.github.com>
2015-08-27 23:17:38 -0400

    Update Challenge #223 [Easy] Garland words.hs

--- Challenge #223 [Easy] Garland words.hs
+++ Challenge #223 [Easy] Garland words.hs
@@ -1,3 +1,7 @@
+{-
+https://www.reddit.com/r/dailyprogrammer/comments/3d4fwj/20150713_challenge_223_easy_garland_words/
+-}
+
 import Data.List
 
 garland word = length . head $ filter (`isPrefixOf` word) (tail $ tails word)
