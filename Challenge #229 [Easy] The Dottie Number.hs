8f0402cc3f450657cbcb66a18598048a40802d33
Joshua Belanger <jcbelanger@users.noreply.github.com>
2015-08-27 23:14:08 -0400

    Update Challenge #229 [Easy] The Dottie Number.hs

--- Challenge #229 [Easy] The Dottie Number.hs
+++ Challenge #229 [Easy] The Dottie Number.hs
@@ -1,3 +1,6 @@
+{-
+https://www.reddit.com/r/dailyprogrammer/comments/3i99w8/20150824_challenge_229_easy_the_dottie_number/
+-}
 converge = until =<< ((==) =<<)
 challenge = converge cos
 optional1 = converge (\x -> x - tan x) 2
