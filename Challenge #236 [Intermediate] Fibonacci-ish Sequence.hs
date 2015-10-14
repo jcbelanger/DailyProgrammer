5888d6352bb7c055d016a846f1405d6efe731902
Joshua Belanger <jcbelanger@users.noreply.github.com>
2015-10-14 16:13:14 -0400

    Update Challenge #236 [Intermediate] Fibonacci-ish Sequence.hs

--- Challenge #236 [Intermediate] Fibonacci-ish Sequence.hs
+++ Challenge #236 [Intermediate] Fibonacci-ish Sequence.hs
@@ -1,3 +1,8 @@
+{-
+https://www.reddit.com/r/dailyprogrammer/comments/3opin7/20151014_challenge_236_intermediate_fibonacciish/
+http://www.maths.surrey.ac.uk/hosted-sites/R.Knott/Fibonacci/fibGen.html
+-}
+
 main = interact (unwords . map show . challenge . read)
 
 challenge x = takeWhile (<=x) $ fib $ div x $ last $ filter ((==0).rem x) $ tail $ takeWhile (<=x) $ fib 1
