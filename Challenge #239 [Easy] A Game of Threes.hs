19662389cd4fc1a1b232dcb2a289b04bfff4b783
Joshua Belanger <jcbelanger@users.noreply.github.com>
2015-11-02 19:34:03 -0500

    Create Challenge #239 [Easy] A Game of Threes.hs

---
+++ Challenge #239 [Easy] A Game of Threes.hs
@@ -0,0 +1,10 @@
+{-
+https://www.reddit.com/r/dailyprogrammer/comments/3r7wxz/20151102_challenge_239_easy_a_game_of_threes/
+-}
+
+threes :: Int -> [[Int]]
+threes 1 = [[1]]
+threes n = let (q, r) = (n + 1) `quotRem` 3 in [n, 1 - r] : threes q
+
+main :: IO ()
+main = interact (unlines . map (unwords . map show) . threes . read)
