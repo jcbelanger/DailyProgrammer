7fefed6c92b12e017ec0db3eb683c7e24361b74d
Joshua Belanger <jcbelanger@users.noreply.github.com>
2015-11-04 15:54:56 -0500

    Create Challenge #239 [Intermediate] A Zero-Sum Game of Threes.hs

---
+++ Challenge #239 [Intermediate] A Zero-Sum Game of Threes.hs
@@ -0,0 +1,42 @@
+{-
+https://www.reddit.com/r/dailyprogrammer/comments/3rhzdj/20151104_challenge_239_intermediate_a_zerosum/
+-}
+
+{-# LANGUAGE DeriveFunctor, BangPatterns #-}
+
+import Data.List
+
+type Threes = [[Integer]]
+
+main :: IO ()
+main = interact (maybe "Impossible" showThrees . challenge . read)
+
+challenge :: Integer -> Maybe Threes
+challenge = find ((==0) . sum . map (!!1) . init) . fastThrees
+
+showThrees :: Threes -> String
+showThrees = unlines . map (unwords . map show)
+
+threes :: (Integer -> [Threes]) -> Integer -> [Threes]
+threes _ 1 = [[[1]]]
+threes f n =
+  [ [n,dn]:after
+  | let (q, r) = n `quotRem` 3
+  , after <- f q
+  , dn <- nub [-r, (-r + 3) `rem` 3]]
+
+fastThrees :: Integer -> [Threes]
+fastThrees = index (threes fastThrees <$> nats)
+
+data Tree a = Tree (Tree a) a (Tree a) deriving (Functor)
+
+index :: Tree a -> Integer -> a
+index (Tree _ m _) 0 = m
+index (Tree l _ r) n = case (n - 1) `quotRem` 2 of
+    (q,0) -> index l q
+    (q,1) -> index r q
+
+nats :: Tree Integer
+nats = go 0 1 where
+  go !n !s = Tree (go l s') n (go r s')
+    where (l, r, s') = (n+s, l+s, s*2)
