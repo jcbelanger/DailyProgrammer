14f0dd52ae7a04fe1c593ebf6f63cc5d20268d13
Joshua Belanger <jcbelanger@users.noreply.github.com>
2015-11-30 10:30:47 -0500

    Update Challenge #243 [Easy] Abundant and Deficient Numbers.hs

--- Challenge #243 [Easy] Abundant and Deficient Numbers.hs
+++ Challenge #243 [Easy] Abundant and Deficient Numbers.hs
@@ -9,8 +9,8 @@ divisors :: Int -> [Int]
 divisors n = [d | d <- [1..n], n `rem` d == 0]
 
 challenge :: Int -> String
-challenge n = case compare delta 0 of
-  LT -> "deficient by " ++ show (abs delta)
-  GT -> "abundant by " ++ show delta
-  EQ -> "perfect"
+challenge n = show n ++ case compare delta 0 of
+  LT -> " deficient by " ++ show (abs delta)
+  GT -> " abundant by " ++ show delta
+  EQ -> " perfect"
   where delta = 2*n - sum (divisors n)
