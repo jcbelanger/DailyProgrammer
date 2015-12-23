bfb4b9134db648f71c89baf26a33fca797e9bfac
Joshua Belanger <jcbelanger@users.noreply.github.com>
2015-12-23 17:24:47 -0500

    Create Challenge # 246 [Intermediate] Letter Splits.hs

---
+++ Challenge # 246 [Intermediate] Letter Splits.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE RecursiveDo #-}
+
+import           Control.Applicative
+import           Data.Char
+import           Data.Foldable
+import           Text.Earley
+
+grammar :: Grammar r (Prod r String Char String)
+grammar = mdo
+  numberWord <- rule $ (:) <$> alphabetP <*> (numberWord <|> pure [])
+  return numberWord
+
+alphabetP :: Prod r String Char Char
+alphabetP = (asum . map letterP) ['A'..'Z']
+
+letterP :: Char -> Prod r String Char Char
+letterP c = c <$ (word . show . toNum) c
+
+toNum :: Char -> Int
+toNum c = ord c - ord 'A' + 1
+
+main :: IO ()
+main = interact $ unlines . fst . fullParses (parser grammar)
