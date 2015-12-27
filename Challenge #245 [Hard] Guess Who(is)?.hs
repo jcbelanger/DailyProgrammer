3e45dfc6565d99a77bb970c39d547e14cad54874
Joshua Belanger <jcbelanger@users.noreply.github.com>
2015-12-27 12:22:26 -0500

    Update Challenge #245 [Hard] Guess Who(is)?.hs

--- Challenge #245 [Hard] Guess Who(is)?.hs
+++ Challenge #245 [Hard] Guess Who(is)?.hs
@@ -2,13 +2,14 @@
 https://www.reddit.com/r/dailyprogrammer/comments/3xdmtw/20151218_challenge_245_hard_guess_whois/
 -}
 
-{-# OPTIONS_GHC -funbox-strict-fields -threaded -rtsopts -O2 #-}
+{-# OPTIONS_GHC -funbox-strict-fields #-}
 
 import           Control.Applicative
 import           Data.Attoparsec.Text
 import           Data.Bits
 import           Data.Ix
 import           Data.List
+import           Data.Map.Strict      (Map)
 import qualified Data.Map.Strict      as M
 import           Data.Monoid
 import           Data.Ord
@@ -17,7 +18,9 @@ import qualified Data.Text.IO         as TIO
 import           Data.Time.Clock
 import           Data.Word
 
-newtype IP = IP4 { ipTo32 :: Word32 } deriving (Eq, Ord, Ix)
+newtype IP = IP4
+  { ipTo32 :: Word32
+  } deriving (Eq, Ord, Ix)
 
 instance Show IP where
   show = intercalate "." . map show . toOctets . ipTo32
@@ -65,23 +68,25 @@ parseQueryFile = many1 (parseIP4 <* endOfLine) <* endOfInput
 
 data Split a = Begin !a | Middle !a !a | End !a deriving (Eq, Ord, Show)
 
-small :: Ord a => Split a -> a
-small (Middle a b) = min a b
-small (Begin a)    = a
-small (End b)      = b
+select :: (a -> a -> a) -> Split a -> a
+select f (Middle a b) = f a b
+select _ (Begin a)    = a
+select _ (End b)      = b
 
 rightOf :: Split a -> Maybe a
 rightOf (Middle _ x) = Just x
 rightOf (Begin x)    = Just x
 rightOf _            = Nothing
 
-newtype Projection a b = Projection (M.Map a (Split b)) deriving (Eq, Ord, Show)
+newtype Projection a b = Projection
+  { splits :: Map a (Split b)
+  } deriving (Eq, Ord, Show)
 
 toProjection :: (Ord k, Ord v) => [((k,k),v)] -> Projection k v
 toProjection = foldl' (\p (k,v) -> insertP k v p) (Projection M.empty)
 
 lookupP :: (Ord a, Ord b) => a -> Projection a b -> Maybe b
-lookupP key (Projection xs) = small <$> atKey <|> nearest where
+lookupP key (Projection xs) = select min <$> atKey <|> nearest where
   (ltKey, atKey, _) = M.splitLookup key xs
   nearest = rightOf . fst =<< M.maxView ltKey
 
@@ -125,28 +130,40 @@ insertP (low,hi) x (Projection xs) = Projection xs' where
                           | otherwise -> withLow
         Nothing                       -> M.insert hi (End x) withLow
 
-challenge :: Projection IP IpBlock -> [IP] -> M.Map (Maybe IpBlock) Int
-challenge projection = foldl' tally M.empty where
-  tally totals ip = M.insertWith (+) (lookupP ip projection) 1 totals
+type MultiSet a = Map a Int
+toMultiSet :: (Traversable t, Ord k) => t (k,Int) -> MultiSet k
+toMultiSet = foldl' (\xs (x,n) -> M.insertWith (+) x n xs) M.empty
 
-showResults :: M.Map (Maybe IpBlock) Int -> String
-showResults counts = unlines
+challenge :: MultiSet IP -> Projection IP IpBlock -> Map IP IpBlock
+challenge counts proj = merged where
+  merged = M.mergeWithKey both onlyCount onlyProj counts (splits proj)
+  both _ _ split = Just (select min split)
+  onlyProj = const M.empty
+  onlyCount between | (root:_) <- M.keys between
+                    , Just result <- lookupP root proj
+                    = M.map (const result) between
+                    | otherwise = M.empty
+
+showResults :: MultiSet IP -> Map IP IpBlock -> String
+showResults counts mapping = unlines
   [ show total ++ " - " ++ maybe "<uknown>" (T.unpack . name) block
-  | (block,total) <- sortOn (Down . snd) (M.assocs counts) ]
+  | let totals = toMultiSet [(M.lookup ip mapping,n) | (ip,n) <- M.assocs counts]
+  , (block,total) <- sortOn (Down . snd) (M.assocs totals) ]
 
 main :: IO ()
 main = do
   parseStart <- getCurrentTime
   Right ips    <- parseOnly parseQueryFile <$> TIO.readFile "query10k.txt"
-  Right blocks <- parseOnly parseBlockFile <$> TIO.readFile "ips300k.txt"
+  Right blocks <- parseOnly parseBlockFile <$> TIO.readFile "ips1mil.txt"
   parseStop <- getCurrentTime
   putStrLn $ "Parse: " ++ show (parseStop `diffUTCTime` parseStart)
   indexStart <- getCurrentTime
+  let counts = toMultiSet $ zip ips (repeat 1)
   let projection = toProjection [(ipRange b, b) | b <- blocks]
-  indexStop <- projection `seq` getCurrentTime
+  indexStop <- counts `seq` projection `seq` getCurrentTime
   putStrLn $ "Index: " ++ show (indexStop `diffUTCTime` indexStart)
   resultsStart <- getCurrentTime
-  let results = challenge projection ips
+  let results = challenge counts projection
   resultsStop <- results `seq` getCurrentTime
   putStrLn $ "Results: " ++ show (resultsStop  `diffUTCTime` resultsStart)
-  putStrLn $ showResults results
+  putStrLn $ showResults counts results
