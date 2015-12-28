28067ade7f72ca37645429dad0b0d6ec2fbc6a35
Joshua Belanger <jcbelanger@users.noreply.github.com>
2015-12-28 18:10:42 -0500

    Update Challenge #247 [Easy] Secret Santa.hs

--- Challenge #247 [Easy] Secret Santa.hs
+++ Challenge #247 [Easy] Secret Santa.hs
@@ -2,18 +2,20 @@
 https://www.reddit.com/r/dailyprogrammer/comments/3yiy2d/20151228_challenge_247_easy_secret_santa/
 -}
 
-import Data.List
-import System.Random
+import           Data.List
+import qualified Data.Set      as S
+import           System.Random
 
 main :: IO ()
 main = do
   gen <- getStdGen
-  interact (fst . selectFrom gen . solutions)
+  interact (maybe "No solution"  fst . selectFrom gen . solutions)
 
-selectFrom :: RandomGen g => g -> [a] -> (a,g)
+selectFrom :: RandomGen g => g -> [a] -> Maybe (a, g)
+selectFrom _   [] = Nothing
 selectFrom gen xs =
   let (ix, gen') = randomR (0, length xs) gen
-  in (xs !! ix, gen')
+  in Just (xs !! ix, gen')
 
 nextWith :: (a -> a -> b) -> [a] -> [b]
 nextWith f xs = zipWith f xs (drop 1 (cycle xs))
@@ -23,8 +25,11 @@ showGives a b = a ++ " -> " ++ b
 
 solutions :: String -> [String]
 solutions input =
-  [ unlines $ nextWith showGives $ map snd attempt
-  | attempt <- permutations [ (fid,name)
+  [ unlines $ nextWith showGives names
+  | attempt <- permutations [ ((fid,nid),name)
                             | (fid,family) <- zip [1..] (lines input)
-                            , name <- words family ]
-  , and $ nextWith (/=) $ map fst attempt]
+                            , (nid,name)   <- zip [1..] (words family) ]
+  , let (ids, names) = unzip attempt
+  , let familyChanges = map fst ids
+  , let peopleChanges = scanl' (flip S.insert) S.empty ids
+  , and $ nextWith (/=) $ zip familyChanges peopleChanges ]
