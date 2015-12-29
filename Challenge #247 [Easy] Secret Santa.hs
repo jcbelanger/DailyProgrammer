e5a5fec4db95eadb16ff3d8702d0d9d9387a74bb
Joshua Belanger <jcbelanger@users.noreply.github.com>
2015-12-29 17:15:44 -0500

    Update Challenge #247 [Easy] Secret Santa.hs

--- Challenge #247 [Easy] Secret Santa.hs
+++ Challenge #247 [Easy] Secret Santa.hs
@@ -2,33 +2,34 @@
 https://www.reddit.com/r/dailyprogrammer/comments/3yiy2d/20151228_challenge_247_easy_secret_santa/
 -}
 
+import           Control.Monad.Random
 import           Data.List
-import qualified Data.Set      as S
-import           System.Random
+import qualified Data.Map              as M
+import           System.Random.Shuffle
 
 main :: IO ()
 main = do
   gen <- getStdGen
-  interact (maybe "No solution"  fst . selectFrom gen . solutions)
+  interact ((`evalRand` gen) . fmap showResults . challenge . toFamilies)
 
-selectFrom :: RandomGen g => g -> [a] -> Maybe (a, g)
-selectFrom _   [] = Nothing
-selectFrom gen xs =
-  let (ix, gen') = randomR (0, length xs) gen
-  in Just (xs !! ix, gen')
+toFamilies :: String -> [[String]]
+toFamilies = map words . lines
 
-nextWith :: (a -> a -> b) -> [a] -> [b]
-nextWith f xs = zipWith f xs (drop 1 (cycle xs))
+showResults :: [String] -> String
+showResults xs = unlines [a++" -> "++b | (a,b) <- zip xs (drop 1 $ cycle xs)]
 
-showGives :: String -> String -> String
-showGives a b = a ++ " -> " ++ b
+challenge :: MonadRandom m => [[String]] -> m [String]
+challenge families = do
+  families' <- shuffleM families
+  let total = sum (map length families)
+      Just (_,(xs,ys)) = M.lookupLE (total `div` 2) (splits families')
+      --TODO allow inter-family-gifting when no perfect split
+  xs' <- shuffleM xs
+  ys' <- shuffleM ys
+  return $ concat [[x,y] | (x,y) <- zip (concat xs') (concat ys')]
 
-solutions :: String -> [String]
-solutions input =
-  [ unlines $ nextWith showGives names
-  | attempt <- permutations [ ((fid,nid),name)
-                            | (fid,family) <- zip [1..] (lines input)
-                            , (nid,name)   <- zip [1..] (words family) ]
-  , let (ids, names) = unzip attempt
-  , and $ nextWith (/=) $ map fst ids
-  , and $ nextWith (/=) $ scanl' (flip S.insert) S.empty ids ]
+splits :: Eq a => [[a]] -> M.Map Int ([[a]],[[a]])
+splits xss = foldl' transferLeft (M.singleton 0 ([],xss)) xss where
+  transferLeft prev cur = M.union prev (nextSplit cur prev)
+  nextSplit cur = M.mapKeysMonotonic (+length cur) . M.map (toLeft cur)
+  toLeft x (as,bs) = (x:as, delete x bs)
