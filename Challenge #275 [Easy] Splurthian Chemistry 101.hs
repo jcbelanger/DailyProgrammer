3ac04b9096c8a9223fa598ea0a2b504c44408080
Joshua Belanger <jcbelanger@users.noreply.github.com>
2016-07-12 12:22:33 -0400

    Update Challenge #275 [Easy] Splurthian Chemistry 101.hs

--- Challenge #275 [Easy] Splurthian Chemistry 101.hs
+++ Challenge #275 [Easy] Splurthian Chemistry 101.hs
@@ -2,17 +2,28 @@
 https://www.reddit.com/r/dailyprogrammer/comments/4savyr/20160711_challenge_275_easy_splurthian_chemistry/
 -}
 
+module Challenge where
+
 import Data.Char
 import Data.List
 
-newtype Element = Element String deriving (Eq, Ord, Show)
-data Symbol = Symbol Char Char deriving (Eq, Ord, Show)
+type Element = String
+type Symbol = String
 
 challenge :: Element -> Symbol -> Bool
-challenge = flip elem . symbols
+challenge = flip elem . splurth
 
-symbols :: Element -> [Symbol]
-symbols (Element name) = [Symbol (toUpper x) (toLower y) | x:xs <- tails name, y:_ <- tails xs]
+splurth :: Element -> [Symbol]
+splurth element = [[toUpper x, toLower y] | x:xs <- tails element, y <- xs]
 
-bonus :: Element -> Symbol
-bonus = minimum . symbols
+bonus1 :: Element -> Symbol
+bonus1 = minimum . splurth
+
+bonus2 :: Element -> Int
+bonus2 = length . nub . splurth
+
+blurth :: Element -> [Symbol]
+blurth element = [toUpper x : map toLower xs | x:xs <- subsequences element]
+
+bonus3 :: Element -> Int
+bonus3 = length . nub . blurth
