33adf4524a5a5daedf8a1da99320c91f26c24104
Joshua Belanger <jcbelanger@users.noreply.github.com>
2016-10-01 16:30:45 -0400

    Update Challenge #285 [Hard] Math Proofs.hs

--- Challenge #285 [Hard] Math Proofs.hs
+++ Challenge #285 [Hard] Math Proofs.hs
@@ -1,23 +1,25 @@
 {-
 https://www.reddit.com/r/dailyprogrammer/comments/557wyy/20160930_challenge_285_hard_math_proofs/
 -}
-
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Control.Applicative
 import           Data.Attoparsec.Text
 import           Data.Bool
 import           Data.Char
-import           Data.Map                (Map)
-import qualified Data.Map                as Map
-import           Data.Text               (Text)
-import qualified Data.Text               as Text
-import qualified Data.Text.IO            as TIO
-import           Text.Parser.Combinators (chainl1)
+import           Data.List
+import           Data.Map                  (Map)
+import qualified Data.Map                  as Map
+import           Data.Ratio
+import           Data.Text                 (Text)
+import qualified Data.Text                 as Text
+import qualified Data.Text.IO              as TIO
+import           Text.Parser.Combinators   (chainl1)
+import           Text.Printf
 
 type Equation = (Expr, Expr)
 data Expr
-    = Lit Integer
+    = Lit Double
     | Var Char
     | Add Expr Expr
     | Sub Expr Expr
@@ -37,10 +39,15 @@ expr = term <|> neg
 term = fact `chainl1` (addFn <|> subFn)
 fact = prim `chainl1` (divFn <|> mulFn)
 prim = lit <|> var <|> parens
-lit = Lit <$> decimal
+lit = Lit <$> unsigned double
 var = Var <$> satisfy isAlpha
-parens = char '(' *> expr <* char ')'
 neg = Neg <$ char '-' <*> expr
+parens = char '(' *> expr <* char ')'
+
+unsigned :: Parser a -> Parser a
+unsigned f = do
+    sign <- optional (char '+' <|> char '-')
+    if sign == Nothing then f else empty
 
 addFn,subFn,mulFn,divFn :: Parser (Expr -> Expr -> Expr)
 addFn = Add <$ char '+'
@@ -48,30 +55,39 @@ subFn = Sub <$ char '-'
 mulFn = Mul <$ optional (char '*')
 divFn = Div <$ char '/'
 
-newtype Polynomial = Polynomial { terms :: Map (Map Char Rational) Rational } deriving (Eq, Ord, Show)
+type Variable = Char
+type Exponent = Rational
+type Coefficient = Rational
+type Terms = Map (Map Variable Exponent) Coefficient
+newtype Polynomial = Polynomial { terms :: Terms } deriving (Eq, Ord)
 
 --Smart constructor to remove trivial terms
-poly :: Map (Map Char Rational) Rational -> Polynomial
+poly :: Terms -> Polynomial
 poly = Polynomial . Map.mapKeys (Map.filter (/=0)) . Map.filter (/=0)
 
 fromExpr :: Expr -> Polynomial
-fromExpr (Neg a)   = fromExpr $ Mul (Lit (-1)) a
-fromExpr (Sub a b) = fromExpr $ Add a (Neg b)
 fromExpr (Lit a)   = poly $ Map.singleton Map.empty (toRational a)
 fromExpr (Var a)   = poly $ Map.singleton (Map.singleton a 1) 1
-fromExpr (Add a b) = fromExpr a `add` fromExpr b
-fromExpr (Mul a b) = fromExpr a `mul` fromExpr b
-fromExpr (Div a b) = fromExpr a `mul` recripPoly (fromExpr b)
+fromExpr (Neg a)   = negateP (fromExpr a)
+fromExpr (Add a b) = fromExpr a `addP` fromExpr b
+fromExpr (Mul a b) = fromExpr a `mulP` fromExpr b
+fromExpr (Sub a b) = fromExpr a `addP` negateP (fromExpr b)
+fromExpr (Div a b) = fromExpr a `mulP` fromDenominator (fromExpr b)
 
-recripPoly :: Polynomial -> Polynomial
-recripPoly = poly . Map.mapKeys (Map.map negate) . Map.map (1/) . terms
+negateP :: Polynomial -> Polynomial
+negateP = poly . Map.map negate . terms
 
-add, mul :: Polynomial -> Polynomial -> Polynomial
-add (Polynomial a) (Polynomial b) = poly $ Map.unionWith (+) a b
-mul (Polynomial a) (Polynomial b) = poly $ Map.fromList
-    [ (Map.unionWith (+) varsA varsB, coefA * coefB)
-    | (varsA, coefA) <- Map.toList a
-    , (varsB, coefB) <- Map.toList b ]
+fromDenominator :: Polynomial -> Polynomial
+fromDenominator = poly . Map.mapKeys (Map.map negate) . Map.map (1/) . terms
+
+addP :: Polynomial -> Polynomial -> Polynomial
+addP (Polynomial a) (Polynomial b) = poly $ Map.unionWith (+) a b
+
+mulP :: Polynomial -> Polynomial -> Polynomial
+mulP (Polynomial a) (Polynomial b) = poly $ Map.fromList
+    [ (Map.unionWith (+) varPowA varPowB, coefA * coefB)
+    | (varPowA, coefA) <- Map.toList a
+    , (varPowB, coefB) <- Map.toList b ]
 
 equivalent :: Equation -> Bool
 equivalent (a,b) = fromExpr a == fromExpr b
