fad74f20c2f26a3d18e83b131d375a8118195267
Joshua Belanger <jcbelanger@users.noreply.github.com>
2016-10-02 10:36:42 -0400

    Update Challenge #285 [Hard] Math Proofs.hs

--- Challenge #285 [Hard] Math Proofs.hs
+++ Challenge #285 [Hard] Math Proofs.hs
@@ -14,11 +14,12 @@ import           Data.Text                 (Text)
 import qualified Data.Text                 as Text
 import qualified Data.Text.IO              as TIO
 import           Text.Parser.Combinators   (chainl1)
+import Data.Ratio
 
 type Equation = (Expr, Expr)
 data Expr
-    = Lit Double
-    | Var Char
+    = Lit Coefficient
+    | Var Variable
     | Add Expr Expr
     | Sub Expr Expr
     | Mul Expr Expr
@@ -57,35 +58,55 @@ type Variable = Char
 type Exponent = Double
 type Coefficient = Double
 type Terms = Map (Map Variable Exponent) Coefficient
-newtype Polynomial = Polynomial { terms :: Terms } deriving (Eq, Ord)
+newtype Polynomial = Polynomial { terms :: Terms } deriving (Eq, Ord, Show)
 
 --Smart constructor to remove trivial terms
 poly :: Terms -> Polynomial
 poly = Polynomial . Map.mapKeys (Map.filter (/=0)) . Map.filter (/=0)
 
-fromExpr :: Expr -> Polynomial
-fromExpr (Lit a)   = poly $ Map.singleton Map.empty  a
-fromExpr (Var a)   = poly $ Map.singleton (Map.singleton a 1) 1
-fromExpr (Neg a)   = negateP (fromExpr a)
-fromExpr (Add a b) = fromExpr a `addP` fromExpr b
-fromExpr (Mul a b) = fromExpr a `mulP` fromExpr b
-fromExpr (Sub a b) = fromExpr a `addP` negateP (fromExpr b)
-fromExpr (Div a b) = fromExpr a `mulP` fromDenominator (fromExpr b)
+instance Num Polynomial where
+    negate = poly . Map.map negate . terms
+    fromInteger = poly . Map.singleton Map.empty . fromInteger
+    (Polynomial a) + (Polynomial b) = poly $ Map.unionWith (+) a b
+    (Polynomial a) * (Polynomial b) = poly $ Map.fromList
+        [ (Map.unionWith (+) varPowA varPowB, coefA * coefB)
+        | (varPowA, coefA) <- Map.toList a
+        , (varPowB, coefB) <- Map.toList b ]
 
-negateP :: Polynomial -> Polynomial
-negateP = poly . Map.map negate . terms
+data PolyRational = PolyRational
+    { prNumerator   :: Polynomial
+    , prDenominator :: Polynomial
+    } deriving (Ord, Show)
+
+instance Eq PolyRational where
+    (PolyRational a b) == (PolyRational c d) = a*d == b*c
+
+instance Num PolyRational where
+    fromInteger a = PolyRational (fromInteger a) 1
+    negate (PolyRational a b) = PolyRational (negate a) b
+    (PolyRational a b) + (PolyRational c d) = PolyRational (a*b*d+c*b*d) (b*d)
+    (PolyRational a b) * (PolyRational c d) = PolyRational (a*c) (b*d)
+
+instance Fractional PolyRational where
+    (PolyRational a b) / (PolyRational c d) = PolyRational (a*d) (b*c)
+
+litP :: Coefficient -> Polynomial
+litP = poly . Map.singleton Map.empty
+
+varP :: Variable -> Polynomial
+varP a = poly $ Map.singleton (Map.singleton a 1) 1
+
+fromExpr :: Expr -> PolyRational
+fromExpr (Lit a)   = PolyRational (litP a) 1
+fromExpr (Var a)   = PolyRational (varP a) 1
+fromExpr (Neg a)   = negate (fromExpr a)
+fromExpr (Add a b) = fromExpr a + fromExpr b
+fromExpr (Mul a b) = fromExpr a * fromExpr b
+fromExpr (Sub a b) = fromExpr a - fromExpr b
+fromExpr (Div a b) = fromExpr a / fromExpr b
 
 fromDenominator :: Polynomial -> Polynomial
 fromDenominator = poly . Map.mapKeys (Map.map negate) . Map.map recip . terms
-
-addP :: Polynomial -> Polynomial -> Polynomial
-addP (Polynomial a) (Polynomial b) = poly $ Map.unionWith (+) a b
-
-mulP :: Polynomial -> Polynomial -> Polynomial
-mulP (Polynomial a) (Polynomial b) = poly $ Map.fromList
-    [ (Map.unionWith (+) varPowA varPowB, coefA * coefB)
-    | (varPowA, coefA) <- Map.toList a
-    , (varPowB, coefB) <- Map.toList b ]
 
 equivalent :: Equation -> Bool
 equivalent (a,b) = fromExpr a == fromExpr b
